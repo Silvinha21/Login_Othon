16e6a0dd0ae6f552af8a57f362659906e935f580
Arthur Silva Soares <202210070002@ifbapa.intra>
2025-07-14 10:10:42 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-card 
+      color="white"
+      class="mx-auto my-8"
+      elevation="16"
+      max-width="344">
+        <v-card-title>Login</v-card-title>
+
+        <v-card-text>
+          Caixa de login
+        </v-card-text>
+
+        <v-text-field>  </v-text-field>
+        <v-card-actions> Confirmar </v-card-actions>
+      </v-card>
+
+      <card-example></card-example>
+
+    </v-container>
+  </v-app>
+</template>
+
+<script setup>
+
+</script>
